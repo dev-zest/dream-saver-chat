3b1d99612edb97112dc6625573f8bd064a41f646
Jsim <jsim@tilda.co.kr>
2024-10-17 11:43:14 +0900

    feat: user input process step 1

--- lib/dream_saver_chat/process_user_input.ex
+++ lib/dream_saver_chat/process_user_input.ex
@@ -16,6 +16,13 @@ defmodule DreamSaverChat.ProcessUserInput do
           res_msg: "목표 금액 #{goal_amount}원이군요! 한 달에 얼마씩 저축하실 계획이신가요?",
           step: step + 1
         }
+      1 ->
+        monthly_savings = String.to_integer(input)
+        %{
+          monthly_savings: monthly_savings,
+          res_msg: "매월 #{monthly_savings}원씩 저축하시는군요. 이자는 단리로 계산할까요, 아니면 월 복리로 계산할까요? (단리/월복리로 대답해주세요)",
+          step: step + 1
+        }
       _ ->
         nil
     end
